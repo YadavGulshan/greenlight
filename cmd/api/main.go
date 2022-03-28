a34f96ff505f2b083db2411e904d08cb83331c85
YadavGulshan <yadavgulshan542@gmail.com>
2022-03-28 21:15:18 +0530

    initial commit with some setup in main file

---
+++ cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+
+const version = "1.0.0"
+
+
+type config struct{
+	port int
+	env string
+}
+
+
+type application struct{
+	config config
+	logger *log.Logger	
+}
+
+func main(){
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yadavgulshan/greenlight
+
+go 1.16
