6216430cbd4126a19467e3f7a50dc4af0a780eb7
YadavGulshan <yadavgulshan542@gmail.com>
2022-03-29 19:23:07 +0530

    added a helper method for returning the interface as the json object in response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	json := `{"status": "available", "environment": %q, "version": %q}`
-	json = fmt.Sprintf(json, app.config.env, version)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(json))
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Server encountered an unexpected error", http.StatusInternalServerError)
+	}
+
 }
