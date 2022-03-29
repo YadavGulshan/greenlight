ce48b37281c6422ee30cbb30e2c230e558669a69
YadavGulshan <yadavgulshan542@gmail.com>
2022-03-29 19:49:36 +0530

    guess what, I don't want to omit the vals

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,8 +6,8 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty"`
-	Genre     []string  `json:"genre,omitempty"`
+	Year      int32     `json:"year"`
+	Runtime   int32     `json:"runtime"`
+	Genre     []string  `json:"genre"`
 	Version   int32     `json:"version"`
 }
